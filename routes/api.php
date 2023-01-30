1b14e7504079422c4dccae8ecf708ab063281da9
lauracat11 <92210196+lauracat11@users.noreply.github.com>
2023-01-30 20:50:32 +0100

    movimiento de ficheros

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MailController;
 
-//Auth
-Route::post('/login', [LoginController::class, 'login'])->middleware('web');
-Route::post('/logout', [LoginController::class, 'logout']);
+// //Auth
+// Route::post('/login', [LoginController::class, 'login'])->middleware('web');
+// Route::post('/logout', [LoginController::class, 'logout']);
 
 
 
 //Mail --Laura
+Route::get('/email', ('emails.form-email'))->name('form-email');
 Route::post('/email', [MailController::class, 'storemail'])->name('email');
