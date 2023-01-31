acd3a928965b78378851cc9ebc4bf4d16e0b00a1
AxselBono <axselbono2@gmail.com>
2023-01-31 17:14:17 +0100

    cambiamos method get-> to delete

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,4 @@ use App\Http\Controllers\JuradoController;
 Route::post('/login', [LoginController::class, 'login'])->middleware('web');
 Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/edicion',[EdicionController::class,'index']);
-Route::get('/jurado/delete/{id}',[JuradoController::class,'destroy']);
+Route::delete('/jurado/delete/{id}',[JuradoController::class,'destroy']);
