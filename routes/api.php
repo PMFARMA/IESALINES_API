1ab11df607fe6e9a4cfe5c9541bc28fe3335b793
eloy411 <castillolopezeloy1@gmail.com>
2023-01-31 18:02:49 +0100

    instalacion el paquete excel

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\EdicionController;
 use App\Http\Controllers\JuradoController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\JuradoController;
+
 //Auth
 Route::post('/login', [LoginController::class, 'login'])->middleware('web');
 Route::post('/logout', [LoginController::class, 'logout']);
