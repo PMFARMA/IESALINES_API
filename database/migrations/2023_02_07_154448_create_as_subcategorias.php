6ebb74b6fb02f3ae3cd707d1a8d0a196fc229ba8
lauracat11 <92210196+lauracat11@users.noreply.github.com>
2023-02-07 16:15:35 +0100

    rename name table

--- database/migrations/2023_02_07_154448_create_as_subcategorias.php
+++ database/migrations/2023_02_07_154448_create_as_subcategorias.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('subcategorias', function (Blueprint $table) {
+        Schema::create('as_subcategorias', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
             $table->string('categoria');
