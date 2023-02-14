8e185f9495939570122f2bdab11170382e7510db
MarcSneivo <Marc.Eslava.Puig@gmail.com>
2023-02-14 15:57:15 +0100

    jurado select

--- app/Http/Controllers/RondasController.php
+++ app/Http/Controllers/RondasController.php
@@ -11,7 +11,9 @@ use Illuminate\Http\Request;
 class RondasController extends Controller
 {
     public function juradoPorcentaje(){
-
+        $jurado = User::select('*')->where('id_edicion','=','25')->join('as_tipojurado','as_jurado.id_tipojurado','as_tipojurado.id')->get();
+        dd($jurado);
+    
     }
 
     public function subCategoriaPorcentaje(){
