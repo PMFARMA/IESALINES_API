40f6765db8c99a4d6d543ab81074eb0f8d4bd3c7
eloy411 <castillolopezeloy1@gmail.com>
2023-02-14 13:52:19 +0100

    controlador Rondas + import models

---
+++ app/Http/Controllers/RondasController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\AuxTipoJuradoSubCat;
+use App\Models\Subcategorias;
+use App\Models\Edicion;
+use Carbon\Carbon;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RondasController extends Controller
+{
+    public function juradoPorcentaje(){
+
+    }
+
+    public function subCategoriaPorcentaje(){
+
+    }
+}
