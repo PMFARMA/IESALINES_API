6be3a422925179ad4c14564733865a53049089bc
eloy castillo <castillolopezeloy1@gmail.com>
2023-03-05 23:22:57 +0100

    añadir premio

--- app/Http/Controllers/VotacionesController.php
+++ app/Http/Controllers/VotacionesController.php
@@ -169,7 +169,7 @@ class VotacionesController extends Controller
             $premio = 'p';
         }
 
-        $obras = EdicionObras::select('id','titulo','id_cod_particip','obras')->where('id_cod_particip', $id)->get();
+        $obras = EdicionObras::select('id','titulo','id_cod_particip','premio')->where('id_cod_particip', $id)->get();
 
         $votosDesierto = Votaciones::selectRaw('count(*) as desierto')->where('id_cod_particip', $id)->whereIn('voto',array('dd',$premioDesierto))->get();
 
@@ -251,3 +251,4 @@ class VotacionesController extends Controller
 
     }
 }
+
